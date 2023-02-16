729eb8df3aa87f2c8e979135df6fb81f18cc0339
Ryuga <ribereo.minami@gmail.com>
2023-02-16 21:59:55 +0900

    2/16勉強

--- Pages/5章_制御構文.xcplaygroundpage/Contents.swift
+++ Pages/5章_制御構文.xcplaygroundpage/Contents.swift
@@ -48,6 +48,14 @@ if let b = optionalB, let c = optionalC {
     print("どちらかの値が存在しません")
 }
 
+// if let , as?演算子による型のダウンキャスト
+
+let any: Any = 1
+
+if let int = any as? Int {
+    print("値はInt型の\(int)です")
+}
+
 
 // guard文 条件不成立時に早期退出する分岐
 
@@ -134,5 +142,87 @@ func guardAdd(_ optionalA: Int?, _ optionalB: Int?) -> Int? {
 }
 
 guardAdd(Optional(1), Optional(2))
+
+
+
+// switch文
+// switch 制御式 {
+//case パターン1:
+// 制御式がパターン1にマッチした場合に実行される文
+//case パターン2:
+// 制御式がパターン2にマッチした場合に実行される文
+//default:
+// 制御式がいづれのパターンにもマッチしなかった場合に実行される文
+//}
+
+let a = 1
+
+switch a {
+case Int.min..<0:
+    print("aは負の値です")
+case 1..<Int.max:
+    print("aは正の値です")
+default:
+    print("aは0です")
+}
+// 「../」半開区間を表す。
+
+enum SomeEnum {
+    case foo
+    case bar
+    case baz
+}
+
+let foo = SomeEnum.foo
+
+switch foo {
+case .foo:
+    print(".foo")
+case .bar:
+    print(".bar")
+case .baz:
+    print(".baz")
+}
+
+
+// whereキーワード
+//switch 制御式 {
+//case パターン where 条件式:
+//    制御式がパターンにマッチし、かつ、条件式を満たす場合に実行される文
+//default:
+//    制御式がいずれのパターンにもマッチしなかった場合に実行される文
+//}
+
+let optionalAA: Int? = 1
+
+switch optionalAA {
+case .some(let a) where a > 10:
+    print("10より大きい値\(a)が存在します")
+default:
+    print("値が存在しない、もしくは10以下の値です。")
+}
+
+
+// ラベル
+
+let value1 = 0 as Any
+
+outerSwitch: switch value1 {
+case let int as Int:
+    let description: String
+    switch int {
+    case 1,3,5,7,9:
+        description = "奇数"
+    case 2,4,6,8,10:
+    description = "偶数"
+    default:
+        print("対象外の値です")
+        break outerSwitch
+    }
+    print("値は\(description)です")
+default:
+    print("対象外の型の値です")
+}
+
 
 //: [Next](@next)
